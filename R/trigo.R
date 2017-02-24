199fc4f1235eaf1d73180eb88bb3b11c4f678f5e
Rintu Kutum <rintukutum@gmail.com>
2017-02-24 12:31:08 +0530

    chanaged man

--- R/trigo.R
+++ R/trigo.R
@@ -1,9 +1,15 @@
 #' Trigonometric functions
 #'
 #' Use angle as input instead of radian, Sin, Cos, Tan
+#' @name trigo
+#' @aliases Sin, Cos, Tan
 #' @param angle Angle \code{angle}
 #' @param digit Digit, default is 10 \code{digit}
 #' @export
+#' @examples
+#' Sin(90)
+#' Cos(90)
+#' Tan(45)
 
 Sin <- function(angle, digit = 10){
 	value <- round(sin(ang2rad(angle)), digit)
