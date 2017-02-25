f9967af95db037c365148c2eeed8da663d53f115
Rintu Kutum <rintukutum@gmail.com>
2017-02-25 13:47:47 +0530

    fire in the building

--- R/donut-prop2arc.R
+++ R/donut-prop2arc.R
@@ -1,20 +1,62 @@
-prop2arc <- function(prop){
-	
+#' Proportion to Angle
+#'
+#' Convert proportion to angles for arc
+#' @param prop Proportion \code{prop}
+#' @export
+#' @examples 
+#' prop <- c(A = 0.1,B = 0.3, C = 0.6)
+#' prop2ang(prop)
+prop2ang <- function(prop){
 	#-----
 	# prop to angle
-	prop <- round(
+	if(length(prop) <= 0){
+		stop('Please provide correct prop value')
+	}
+
+	if(sum(prop) != 1){
+		stop('Please provide prop value 0 to 1')
+	}
+	if(length(prop) == 1){
+		angs_arc <- c(
+			s.ang = 0,
+			e.ang = 360
+		)
+		names(angs_arc) <- names(prop)
+		return(angs_arc)
+	}else{
+		#----
+		# sort before split
+		prop <- sort(round(
 		unlist(prop),
 		digit = 4
+		))
+	
+		angs <- round(
+			seq(
+				0,
+				360,
+				length.out = 360
+				),
+			digit = 2
+			)
+		props <- round(
+			seq(
+				0,
+				1,
+				length.out = 360),
+			digit = 4
+			)
+		bins <- as.factor(
+			findInterval(
+				props,
+				as.numeric(prop)
+			)
 		)
-	legend_text <- names(prop)
-	angs <- round(seq(
-		0,
-		360,
-		length.out = 3600),
-		digit = 2)
-	props <- round(seq(
-		0,
-		1,
-		length.out = 3600),
-		digit = 4)
+		bin_angs <- split(angs, bins)
+		#----
+		# use the custom function to get only start and end
+		angs_arc <- get_angs_arc(bin_angs)
+		# names(angs_arc) <- names(prop)
+		return(angs_arc)	
+	}
 }
--- R/donut-utils.R
+++ R/donut-utils.R
@@ -3,4 +3,23 @@ get_prop <- function(var){
 	return(prop)
 }
 
+get_angs_arc <- function(bin_angs){
+	len_bins <- length(bin_angs) - 1
+	angs_arc <- list()
+	for(i in 1:len_bins){
+		s.ang <- bin_angs[[i]][1]
+		e.ang <- bin_angs[[i+1]][1]
+		if(i == len_bins){
+			# last bin
+			x_angs <- bin_angs[[len_bins+1]]
+			len.bin_ang <- length(x_angs)
+			e.ang <- x_angs[len.bin_ang]
+		}
+		angs_arc[[i]] <- c(
+			s.ang = s.ang,
+			e.ang = e.ang
+		)
+	}
+	return(angs_arc)
+}
 
